f2151e087bc86d5894861a1426620b969feec995
Elriamk2 <47573423+Elriamk2@users.noreply.github.com>
2019-12-16 23:05:27 +0000

    creating lists and classes
    changing inputs to lists, created a class but don't know how to use them yet

--- current_income_expenses.py
+++ current_income_expenses.py
@@ -7,6 +7,7 @@ Create expenses calculator
 '''
 
 import sys
+import re
 
 # calculating the costs of commuting  and living away from home
 
@@ -174,7 +175,7 @@ def car_travel_per_trip(distance = 0):
         weekly_car_allowance = 0
     return weekly_car_allowance
 
-def train_ticket_return(train_ticket_return = 0):
+def train_ticket_return(train_ticket_return = 0, train_trips = 0):
     """Calculates the weekly costs of a train ticket """
     if train_ticket_return == "":
         print("You have no train expenses")
@@ -183,12 +184,12 @@ def train_ticket_return(train_ticket_return = 0):
         print("You have no train expenses")
         weekly_train_allowance = 0
     elif train_ticket_return > 0:
-        weekly_train_allowance = (train_ticket_return * train_trips_per_week)
+        weekly_train_allowance = (train_ticket_return * train_trips)
         print("Your weekly train ticket costs are:", weekly_train_allowance)
     else:
         print("You have no train expenses")
         weekly_train_allowance = 0
-        calculations_list[5] = train_ticket_return
+        weekly_train_allowance = train_ticket_return
     return weekly_train_allowance
 
 # Housing costs
@@ -200,37 +201,34 @@ def housing_payments(housing_payments = 0):
     else:
         print("You have no mortgage expenses")
         weekly_housing_costs = 0
-    calculations_list[7] = housing_payments
+    weekly_housing_costs = housing_payments
     return weekly_housing_costs
 
 def council_tax_band(council_tax_band = 0):
     council_tax_calc = council_tax_band.upper()
     """Calculates your council tax based on the  """
-    if council_tax_calc == "":
-        print("You have not entered any council tax and the calculcation assumes this is 0")
-        weekly_council_tax = 0
-    elif council_tax_band =="0":
+    if council_tax_band =="0":
         print("You are not currently paying council tax")
         weekly_council_tax = 0
-    elif council_tax_calc == "A*":
+    elif council_tax_calc.upper() == "A*":
         weekly_council_tax = 932.26 / 52.090714
-    elif council_tax_calc == "A":
+    elif council_tax_calc.upper() == "A":
         weekly_council_tax = 1118.71 / 52.090714
-    elif council_tax_calc == "B":
+    elif council_tax_calc.upper() == "B":
         weekly_council_tax = 1305.17 / 52.090714
-    elif council_tax_calc == "C":
+    elif council_tax_calc.upper() == "C":
         weekly_council_tax = 1491.62 / 52.090714
-    elif council_tax_calc == "D":
+    elif council_tax_calc.upper() == "D":
         weekly_council_tax = 1678.07 / 52.090714
-    elif council_tax_calc == "E":
+    elif council_tax_calc.upper() == "E":
         weekly_council_tax = 2164.08 / 52.090714 
-    elif council_tax_calc == "F":
+    elif council_tax_calc.upper() == "F":
         weekly_council_tax = 2646.65 / 52.090714
-    elif council_tax_calc == "G":
+    elif council_tax_calc.upper() == "G":
         weekly_council_tax = 3,156.65 / 52.090714
     else:
         weekly_council_tax = 3911.36 / 52.090714
-    calculations_list[6] = council_tax_band
+#    calculations_list[6] = council_tax_band
     return float(weekly_council_tax)
     print("Your weekly council tax payments at band", council_tax_calc, " are", round(weekly_council_tax, 2))
 
@@ -245,7 +243,7 @@ def monthly_electricity_bill(electric_bill = 0):
     else:
         electric_bill = 0
         print("You have entered an invalid figure")
-    calculations_list[8] = electric_bill
+    weekly_electric_bill = electric_bill
     return weekly_electric_bill
 
 def monthly_telephony_bill(telephony = 0):
@@ -258,5 +256,5 @@ def monthly_telephony_bill(telephony = 0):
     else:
         telephony_bill = 0
         print("You have entered an invalid figure")
-    calculations_list[9] = telephony
-    return telephony_bill    
+    telephony_bill = telephony
+    return telephony_bill     
--- expense_calc_inputs.py
+++ expense_calc_inputs.py
@@ -1,77 +1,90 @@
 from current_income_expenses import *
 
 
+class national_insurance:
+    def __init__(self, gross_pay):
+        self.gross_pay = gross_pay
+
+    def __repr__(self):
+        return f({self.gross_pay})
+
+
+
 # value storage list?
 # current_wages gross_pay, select_tax_region, car_distance, car_trips, train_ticket_return, train_trips_per_week, council_tax_assesment, housing_payments, monthly_telephony_bill
 calculations_list = [0,0,0,0,0,0,0,0,0,0]
 
 # Basis for differential wage calculation
-current_wages = (float(input("What is your current salary? :")))
-calculations_list[0] = current_wages
+# calculations_list[0] = (float(input("What is your current salary? :")))
+# calculations_list[0] = current_wages
 
 while True:
     try:
-        gross_pay = (float(input("Enter the wage you wish to calculate: ")))
-        calculations_list[1] = gross_pay
+        gross_pay = (input("Enter the wage you wish to calculate: "))
+        calculations_list[0] = gross_pay
         break
     except:
         print("You must enter your salary as a numeric value")
         sys.exit(1)
 
-select_tax_region = (input("If you live in Scotland press 1, if you live in the rest of the UK press 2: "))
-if select_tax_region == '1':
+calculations_list[1] = (input("If you live in Scotland press 1, if you live in the rest of the UK press 2: "))
+if calculations_list[1] == '1':
     weekly_income = yearly_wages_scot(gross_pay)
-    current_weekly_income = yearly_wages_scot(current_wages)
-elif select_tax_region == '2':
+    current_weekly_income = yearly_wages_scot(calculations_list[1])
+elif calculations_list[1] == '2':
     weekly_income = yearly_wages_uk_other(gross_pay)
-    current_weekly_income = yearly_wages_uk_other(current_wages)
+    current_weekly_income = yearly_wages_uk_other((float(calculations_list[0])))
 else:
     weekly_income = yearly_wages_uk_other(gross_pay)
-    current_weekly_income = yearly_wages_uk_other(current_wages)
+    current_weekly_income = yearly_wages_uk_other((float(calculations_list[0])))
 
 #test_wages_scot()
 
 # define the travel to work / secondary location costs
-car_distance = (float(input("What is the return trip distance? If you are not using a car enter 0: ")))
-car_trips = (float(input("How many trips do you make? If you are not using a car enter 0: ")))
-calculate_car_travel = car_distance * car_trips
-calculations_list[2] = car_distance
-calculations_list[3] = car_trips
+calculations_list[1] = (input("What is the return trip distance? If you are not using a car enter 0: "))
+calculations_list[2] = (input("How many trips do you make? If you are not using a car enter 0: "))
+calculate_car_travel = float(calculations_list[2]) * float(calculations_list[3])
+# calculations_list[2] = car_distance
+# calculations_list[3] = car_trips
 
 weekly_car_payments = car_travel_per_trip(calculate_car_travel)
 
 # define the train travel costs
 # train_ticket_return(4.70)
-train_trips_per_week = float(input("How many return train trips do you take per week? Enter 0 if not using a train: "))
-train_ticket_return(float(input("How much does your train ticket cost? Enter 0 if not using a train: ")))
-calculations_list[4] =  train_trips_per_week
-#calculations_list[5] = train_ticket_return(train_ticket_return)
+calculations_list[3] = input("How many return train trips do you take per week? Enter 0 if not using a train: ")
+calculations_list[4] = input("How much does your train ticket cost? Enter 0 if not using a train: ")
+#calculations_list[4] =  train_trips_per_week
+weekly_train_costs = train_ticket_return(float(calculations_list[3]), float(calculations_list[4]))
 
 
 # Calculate housing costs, council tax band should be entered as a string
 # council_tax_band("b")
-council_tax_assesment = input("What council tax band is your house? Enter 0 if you are not paying council tax: ")
-weekly_council_payments = float(council_tax_band(council_tax_assesment))
+calculations_list[6] = input("What council tax band is your house? Enter 0 if you are not paying council tax: ")
+weekly_council_payments = council_tax_band(float(calculations_list[6]))
 #calculations_list[6] = council_tax_assesment(council_tax_assesment)
 
 # housing_payments(156)
-weekly_housing_costs = housing_payments(float(input("How much is your mortgage or rent? Enter 0 if you are not paying these: ")))
-# calculations_list[7] = housing_payments(housing_payments)
+calculations_list[7] = housing_paymentsinput("How much is your mortgage or rent? Enter 0 if you are not paying these: ")
+weekly_housing_costs = housing_payments(float(calculations_list[7]))
 
 # monthly_electricity_bill(55)
-weekly_electric_bill = monthly_electricity_bill(float(input("How much will you be paying per month for electricity? If no electic bill enter 0: ")))
+calculations_list[8] = monthly_electricity_bill(input("How much will you be paying per month for electricity? If no electic bill enter 0: "))
 #calculations_list[8] = monthly_electricity_bill
+weekly_electric_bill = float(monthly_electricity_bill)
+
+# monthly_gas_bill 
 
 # monthly_broadband = 32.99
-weekly_telephony = float(input("How much will you pay for telephony services monthly? If you are not paying for these enter 0: "))
-monthly_telephony_bill(weekly_telephony)
+calculations_list[9] =input("How much will you pay for telephony services monthly? If you are not paying for these enter 0: ")
+monthly_telephony_bill(float(weekly_telephony))
 # calculations_list[9] = weekly_telephony(weekly_telephony)
+weekly_telephony = float(monthly_telephony_bill)
 
 #additional income streams
 additional_monthly_income = float(input("Enter any additional monthly income, if none, enter 0: "))
 
 # calculate the weekly outgoings
-weekly_take_home = weekly_income - weekly_car_payments - weekly_council_payments - weekly_housing_costs - weekly_electric_bill - weekly_telephony + (additional_monthly_income / 4.35) # - weekly_gas_bill
+weekly_take_home = weekly_income - weekly_car_payments - weekly_council_payments - weekly_train_costs - weekly_housing_costs - weekly_electric_bill - weekly_telephony + (additional_monthly_income / 4.35) # - weekly_gas_bill
 monthly_take_home = weekly_take_home * 4.35
 yearly_take_home = weekly_take_home * 52.1
 current_yearly_take_home = current_weekly_income * 52.1
